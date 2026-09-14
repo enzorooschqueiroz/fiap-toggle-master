a3bbd44bda4bb606da226db9ac54f792f002c8d8
enzorooschqueiroz <enzoroosch@gmail.com>
2026-09-14 15:08:16 -0300

    chore(terraform): use S3 native lock (use_lockfile) instead of deprecated dynamodb_table

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "togglemaster-tfstate-185796529499"
-    key            = "toggle-master/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
+    bucket      = "togglemaster-tfstate-185796529499"
+    key         = "toggle-master/terraform.tfstate"
+    region      = "us-east-1"
+    use_lockfile = true
+    encrypt     = true
   }
 }
